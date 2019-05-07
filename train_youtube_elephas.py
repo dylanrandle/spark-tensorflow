697dc54a3e4b15bdcac40536db47b2d772e9aef7
Dylan Randle <dylanrandle@rogers.com>
2019-05-06 21:06:10 -0400

    update train script to include test score

--- train_youtube_elephas.py
+++ train_youtube_elephas.py
@@ -8,10 +8,8 @@ from create_youtube_model import create_model
 # ============
 # SPARK SETUP
 # ============
-
 # NOTE: Run the below command when submitting this job as well:
 # spark-submit --jars ecosystem/spark/spark-tensorflow-connector/target/spark-tensorflow-connector_2.11-1.10.0.jar train_youtube_elephas.py
-
 conf = SparkConf().setAppName('Youtube-8M') \
                   .set("spark.jars",
                        "ecosystem/spark/spark-tensorflow-connector/target/spark-tensorflow-connector_2.11-1.10.0.jar")
@@ -21,24 +19,23 @@ spark = pyspark.sql.SparkSession(sc)
 # =============
 # READING DATA
 # =============
-
 # We use the Tensorflow-Spark connector (provided by Tensorflow)
 # which allows us to read the tfrecord files into a Spark
 # DataFrame.
-
-# VIDEO-LEVEL
 vid_train_df = spark.read.format("tfrecords").option("recordType", "Example").load('s3://cs205-youtube-data/yt8pm/v2/video/train*.tfrecord')
-# vid_test_df = spark.read.format("tfrecords").option("recordType", "Example").load('s3://cs205-youtube-data/yt8pm/v2/video/test*.tfrecord')
+vid_test_df = spark.read.format("tfrecords").option("recordType", "Example").load('s3://cs205-youtube-data/yt8pm/v2/video/validate*.tfrecord')
 
 # ==============
 # PREPROCESSING
 # ==============
-
 # 1) take mean_rgb features
 # 2) take top 10 classes
 train_df = vid_train_df.select('mean_rgb', 'labels')
+test_df = vid_test_df.select('mean_rgb', 'labels')
 train_rdd = train_df.rdd
+test_rdd = test_df.rdd
 train_rdd = train_rdd.map(lambda x: (x[0], x[1]))
+test_rdd = test_rdd.map(lambda x: (x[0], x[1]))
 def convert_labels(labels):
     allowed=np.arange(10)
     one_hot=np.zeros(10)
@@ -47,17 +44,19 @@ def convert_labels(labels):
             one_hot[l]=1
     return one_hot
 train_rdd = train_rdd.map(lambda x: (np.array(x[0]), convert_labels(x[1])))
+test_rdd = test_rdd.map(lambda x: (np.array(x[0]), convert_labels(x[1])))
 
 # =========
 # TRAINING
 # =========
-
 keras_model = create_model()
-spark_model = SparkModel(keras_model, frequency='batch', mode='synchronous')
+spark_model = SparkModel(keras_model, frequency='batch', mode='asynchronous')
 history = spark_model.fit(train_rdd, epochs=1, batch_size=32, verbose=2)
 
 # =========
 # TESTING
 # =========
-# score = spark_model.master_network.evaluate(x_test, y_test, verbose=2)
-# print('Test accuracy:', score[1])
+x_test = np.array(test_rdd.map(lambda x: x[0]).collect())
+y_test = np.array(test_rdd.map(lambda x: x[1]).collect())
+score = spark_model.master_network.evaluate(x_test, y_test, verbose=2)
+print('Test accuracy:', score[1])
